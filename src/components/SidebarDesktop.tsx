96caf18e05f0e6a9709b58959945a37a3e250527
Dewa <108676698+dwmd12@users.noreply.github.com>
2023-07-31 11:37:40 +0700

    warehouse add table + staticprop

--- src/components/SidebarDesktop.tsx
+++ src/components/SidebarDesktop.tsx
@@ -11,63 +11,58 @@ const SidebarDesktop = () => {
     }
 
     return (
-        <div className='flex justify-between pr-6'>
-            <div className='bg-cardBlack w-1/5 pt-8 h-screen px-3'>
-                {/* Warehouse */}
-                <button
-                    onClick={() => {
-                        changeSide('warehouse');
-                        router.push('/warehouse');
-                    }}
-                    className={`flex transition-all w-full justify-start gap-10 items-center rounded-lg px-5 py-3 hover:bg-cardGray hover:-translate-y-1 ${
-                        navbarValue == 'warehouse' ? 'bg-cardGray' : ''
-                    }`}
-                >
-                    <img src='/warehouse.png' width={35} alt='' />
-                    <h4 className='text-lg'>Warehouse</h4>
-                </button>
-                {/* Incoming */}
-                <button
-                    onClick={() => {
-                        changeSide('incoming');
-                        router.push('/incoming');
-                    }}
-                    className={`flex transition-all w-full justify-start gap-10 items-center rounded-lg px-5 py-3 hover:bg-cardGray hover:-translate-y-1 ${
-                        navbarValue == 'incoming' ? 'bg-cardGray ' : ''
-                    } `}
-                >
-                    <img src='/incoming.png' width={35} alt='' />
-                    <h4 className='text-lg'>Incoming</h4>
-                </button>
-                {/* Outgoing */}
-                <button
-                    onClick={() => {
-                        changeSide('outgoing');
-                        router.push('/outgoing');
-                    }}
-                    className={`flex transition-all w-full justify-start gap-10 items-center rounded-lg px-5 py-3 hover:bg-cardGray hover:-translate-y-1 ${
-                        navbarValue == 'outgoing' ? 'bg-cardGray' : ''
-                    } `}
-                >
-                    <img src='/warehouse.png' width={35} alt='' />
-                    <h4 className='text-lg'>Outgoing</h4>
-                </button>
-                <button
-                    onClick={() => {
-                        changeSide('outgoing');
-                        router.push('/item');
-                    }}
-                    className={`flex transition-all w-full justify-start gap-10 items-center rounded-lg px-5 py-3 hover:bg-cardGray hover:-translate-y-1 ${
-                        navbarValue == 'item' ? 'bg-cardGray' : ''
-                    } `}
-                >
-                    <img src='/item.png' width={35} alt='' />
-                    <h4 className='text-lg'>Item</h4>
-                </button>
-            </div>
-            <div className=''>
-                <h2>Testing</h2>
-            </div>
+        <div className='bg-cardBlack w-1/5 pt-8 h-screen flex flex-col gap-4 px-3'>
+            {/* Warehouse */}
+            <button
+                onClick={() => {
+                    changeSide('warehouse');
+                    router.push('/warehouse');
+                }}
+                className={`flex transition-all w-full justify-start gap-10 items-center rounded-lg px-5 py-3 hover:bg-cardGray hover:-translate-y-1 ${
+                    navbarValue == 'warehouse' ? 'bg-cardGray font-bold' : ''
+                }`}
+            >
+                <img src='/warehouse.png' width={35} alt='' />
+                <h4 className='text-lg'>Warehouse</h4>
+            </button>
+            {/* Incoming */}
+            <button
+                onClick={() => {
+                    changeSide('incoming');
+                    router.push('/incoming');
+                }}
+                className={`flex transition-all w-full justify-start gap-10 items-center rounded-lg px-5 py-3 hover:bg-cardGray hover:-translate-y-1 ${
+                    navbarValue == 'incoming' ? 'bg-cardGray font-bold ' : ''
+                } `}
+            >
+                <img src='/incoming.png' width={35} alt='' />
+                <h4 className='text-lg'>Incoming</h4>
+            </button>
+            {/* Outgoing */}
+            <button
+                onClick={() => {
+                    changeSide('outgoing');
+                    router.push('/outgoing');
+                }}
+                className={`flex transition-all w-full justify-start gap-10 items-center rounded-lg px-5 py-3 hover:bg-cardGray hover:-translate-y-1 ${
+                    navbarValue == 'outgoing' ? 'bg-cardGray font-bold' : ''
+                } `}
+            >
+                <img src='/warehouse.png' width={35} alt='' />
+                <h4 className='text-lg'>Outgoing</h4>
+            </button>
+            <button
+                onClick={() => {
+                    changeSide('outgoing');
+                    router.push('/item');
+                }}
+                className={`flex transition-all w-full justify-start gap-10 items-center rounded-lg px-5 py-3 hover:bg-cardGray hover:-translate-y-1 ${
+                    navbarValue == 'item' ? 'bg-cardGray font-bold' : ''
+                } `}
+            >
+                <img src='/item.png' width={35} alt='' />
+                <h4 className='text-lg'>Item</h4>
+            </button>
         </div>
     );
 };
--- src/pages/warehouse.tsx
+++ src/pages/warehouse.tsx
@@ -2,6 +2,9 @@ import Navbar from '@/components/Navbar';
 import NavbarPhone from '@/components/NavbarPhone';
 import CardWarehouse from '@/components/CardWarehouse';
 import { useContext, useEffect, useState } from 'react';
+import type { InferGetStaticPropsType, GetStaticProps, NextPage } from 'next';
+import { Prisma } from '@prisma/client';
+import { prisma } from '../../db/client';
 
 import {
     Select,
@@ -11,12 +14,40 @@ import {
     SelectValue,
 } from '@/components/ui/select';
 
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table';
+
 import SidebarDesktop from '@/components/SidebarDesktop';
 
-const Warehouse = () => {
+async function getWarehouse() {
+    const warehouseItems = await prisma.warehouse_table.findMany();
+    return warehouseItems;
+}
+
+export const getStaticProps: GetStaticProps<{
+    warehouseItems: Prisma.PromiseReturnType<typeof getWarehouse>;
+}> = async () => {
+    const warehouseItems = await getWarehouse();
+
+    return {
+        props: {
+            warehouseItems,
+        },
+        // revalidate: 1,
+    };
+};
+
+const Warehouse: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (
+    props: InferGetStaticPropsType<typeof getStaticProps>
+) => {
     const [windowWidth, setWindowWidth] = useState<number>(0);
-
-
 
     useEffect(() => {
         function handleResize() {
@@ -32,10 +63,110 @@ const Warehouse = () => {
     return (
         <main className='min-h-screen bg-bgBlack text-white font-outfit'>
             {windowWidth >= 700 ? (
-                // <h1>testing</h1>
                 <div>
                     <Navbar isWarehouse={true}></Navbar>
-                    <SidebarDesktop></SidebarDesktop>
+                    <div className='flex justify-between pr-6'>
+                        <SidebarDesktop></SidebarDesktop>
+                        {/* Main Screen */}
+                        <div className='w-4/5 flex flex-col px-10'>
+                            {/* Button Container */}
+                            <div className='flex justify-around'>
+                                <div className='bg-cardGray w-1/5 rounded-lg flex justify-between items-center p-3'>
+                                    <img
+                                        src='/warehouse.png'
+                                        className='w-8 h-8'
+                                        alt=''
+                                    />
+                                    <div>
+                                        <h5 className='text-gray'>Warehouse</h5>
+                                        <h4 className=''>14</h4>
+                                    </div>
+                                </div>
+                                <div className='bg-cardGray w-1/5 rounded-lg flex justify-between items-center p-3'>
+                                    <img
+                                        src='/incoming.png'
+                                        className='w-8 h-8'
+                                        alt=''
+                                    />
+                                    <div>
+                                        <h5 className='text-gray'>
+                                            Incoming Item
+                                        </h5>
+                                        <h4 className=''>14</h4>
+                                    </div>
+                                </div>
+                                <div className='bg-cardGray w-1/5 rounded-lg flex justify-between items-center p-3'>
+                                    <img
+                                        src='/outgoing.png'
+                                        className='w-8 h-8'
+                                        alt=''
+                                    />
+                                    <div>
+                                        <h5 className='text-gray'>
+                                            Outgoing item
+                                        </h5>
+                                        <h4 className=''>14</h4>
+                                    </div>
+                                </div>
+                                <div className='bg-cardGray w-1/5 rounded-lg flex justify-between items-center p-3'>
+                                    <img
+                                        src='/item.png'
+                                        className='w-8 h-8'
+                                        alt=''
+                                    />
+                                    <div>
+                                        <h5 className='text-gray'>
+                                            Total Item
+                                        </h5>
+                                        <h4 className=''>14</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* Warehouse */}
+                            <div className=' h-fit p-5 w-fit text-lg mt-5 bg-cardBlack border-bluePrimary border-2 rounded-lg'>
+                                <h3 className='text-center font-outfit font-bold text-white '>
+                                    Warehouse Items
+                                </h3>
+                                <Table className='text-white font-outfit'>
+                                    <TableCaption>
+                                        A list of all{' '}
+                                        <span className='font-bold'>
+                                            Warehouse
+                                        </span>{' '}
+                                        items.
+                                    </TableCaption>
+                                    <TableHeader>
+                                        <TableRow className='text-gray hover:bg-cardBlack'>
+                                            <TableHead className=''>
+                                                ID
+                                            </TableHead>
+                                            <TableHead className=''>
+                                                Quantity
+                                            </TableHead>
+                                            <TableHead className=''>
+                                                Item ID
+                                            </TableHead>
+                                        </TableRow>
+                                    </TableHeader>
+                                    <TableBody>
+                                        {props.warehouseItems.map((prop) => (
+                                            <TableRow key={prop.warehouse_id}>
+                                                <TableCell className=''>
+                                                    {prop.warehouse_id}
+                                                </TableCell>
+                                                <TableCell className=''>
+                                                    {prop.warehouse_quantity}
+                                                </TableCell>
+                                                <TableCell className=''>
+                                                    {prop.item_id}
+                                                </TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             ) : (
                 <>
