93a0e7c9a5c63ff880db16bdbd033848a3337faa
Dewa made <108676698+DewaMadeWira@users.noreply.github.com>
2023-08-15 17:08:48 +0700

    update lucide package

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     modularizeImports: {
         'lucide-react': {
-            transform: 'lucide-react/dist/esm/icons/{{member}}',
+            transform: 'lucide-react/dist/esm/icons/{{kebabCase member}}',
         },
     },
 };
